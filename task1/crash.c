e187146c9a4cc5281f16526b5ceb2959bda99366
Rui Kang Na <rkna02@ssh-linux3.ece.ubc.ca>
2022-03-20 19:03:03 -0700

    final implementation

--- task1/crash.c
+++ task1/crash.c
@@ -4,13 +4,28 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define MAXLINE 1024
 #define MAXJOBS 1024
 
+void cmd_jobs(const char**);
+void insert_jobs(const char**, pid_t);
+
 char **environ;
 
 // TODO: you will need some data structure(s) to keep track of jobs
+// Job object consisting job id, pid, and name of command
+typedef struct {
+    int id;
+    pid_t pid;
+    char *name;
+    //bool present;
+} job;
+
+job jobs[1024];  // array of pointers to job objects
+job curr;
+int job_id = 0;  // job id reference numbers
 
 void handle_sigchld(int sig) {
     // TODO
@@ -42,24 +57,41 @@ void spawn(const char **toks, bool bg) { // bg is true iff command ended with &
     }
 
     if (p1 == 0) {
+
         int success = execvp(toks[0], toks);
         if (success == -1) {
             fprintf(stderr, "ERROR: cannot run %s\n", toks[0]);
             exit(0);
-        }
+        } 
+
     } else {
+        job_id = job_id + 1;
+        insert_jobs(toks, p1);
+        printf("[%i] (%ld)  %s\n", job_id, (long) p1, jobs[job_id - 1].name);
         if (bg) {
-            pid_t p2 = waitpid(-1, NULL, WNOHANG);
+            return;
         } else {
             pid_t p2 = waitpid(p1, NULL, 0);
         }
     }
 }
 
+void insert_jobs(const char **toks, pid_t pid) {
+    curr.id = job_id;
+    curr.pid = pid;
+    char *copy;
+    copy = malloc((strlen(toks[0]) + 1));
+    strcpy(copy, toks[0]);
+    curr.name = copy;
+    jobs[job_id - 1] = curr;  
+}
 
 void cmd_jobs(const char **toks) {
     // TODO
-
+    for (int i = 0; i < job_id; i++) {
+        printf("[%i] (%ld)  running  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+    }
+    
 }
 
 void cmd_fg(const char **toks) {
@@ -88,8 +120,15 @@ void eval(const char **toks, bool bg) { // bg is true iff command ended with &
     if (strcmp(toks[0], "quit") == 0) {
         cmd_quit(toks);
     // TODO: other commands
+    } else if (strcmp(toks[0], "jobs") == 0) {
+        if (toks[1] != NULL) {
+            fprintf(stderr, "ERROR: jobs takes no arguments\n");
+        } else {
+            cmd_jobs(toks);
+        }
     } else {
         spawn(toks, bg);
+        //cmd_jobs(toks);
     }
 }
 
@@ -104,7 +143,7 @@ void parse_and_eval(char *s) {
         int t = 0;
 
         while (*s != '\0' && !end) {
-            while (*s == ' ' || *s == '\t' || *s == '\n') ++s;
+            while (*s == ' ' || *s == '\t' || *s == '\n') ++s;  
             if (*s != '&' && *s != ';' && *s != '\0') toks[t++] = s;
             while (strchr("&; \t\n", *s) == NULL) ++s;
             switch (*s) {
