919eeaff39e37c27afbdd656073823676a959784
Rui Kang Na <rkna02@ssh-linux3.ece.ubc.ca>
2022-03-24 10:20:10 -0700

    task 3 4 draft

--- task3/crash.c
+++ task3/crash.c
@@ -4,16 +4,46 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define MAXLINE 1024
 #define MAXJOBS 1024
 
+void cmd_jobs(const char**);
+void insert_jobs(const char**, pid_t);
+
 char **environ;
 
 // TODO: you will need some data structure(s) to keep track of jobs
+// Job object consisting job id, pid, and name of command
+typedef struct {
+    char *id_s;
+    int id;
+    char *pid_s;
+    pid_t pid;
+    char *name;
+    bool terminated;
+} job;
+
+job jobs[1024];  // array of pointers to job objects
+job curr;
+int job_id = 0;  // job id reference numbers
 
 void handle_sigchld(int sig) {
-    // TODO
+    pid_t sig_pid;
+    sigset_t mask;  // sigset_t represent a signal set
+    sigemptyset(&mask);  // all recongnized signals are excluded
+    sigaddset(&mask, SIGCHLD);  // adds the SIGCHLD signal into the set to include it
+    while ((sig_pid = waitpid(-1, 0, WNOHANG)) != -1) {
+        for (int i = 0; i < job_id; i++) {
+            if (jobs[i].pid == sig_pid) {
+                sigprocmask(SIG_BLOCK, &mask, NULL);
+                jobs[i].terminated = true;
+                sigprocmask(SIG_UNBLOCK, &mask, NULL);
+            }
+        }
+    }
 }
 
 void handle_sigtstp(int sig) {
@@ -21,7 +51,27 @@ void handle_sigtstp(int sig) {
 }
 
 void handle_sigint(int sig) {
-    // TODO
+    /*
+    int length = (int) strlen(jobs[0].id_s) + (int) strlen(jobs[0].pid_s) + (int) strlen(jobs[0].name);
+    length = length + 54;
+    printf("%ld", (long) jobs[0].pid_s);
+    char output[length];
+    strcpy(output, "[");
+    strcat(output, jobs[0].id_s);
+    strcat(output, "] (");
+    strcat(output, jobs[0].pid_s);
+    strcat(output, ")  killed  ");
+    strcat(output, jobs[0].name);
+    strcat(output, "\n");
+    
+
+    write(STDOUT_FILENO, output, (size_t) strlen(output));
+    */
+    write(1, "(", 2);
+    write(1, jobs[0].id_s, 5);
+    write(1, ")", 2);
+    write(1, jobs[0].pid_s, 10);
+    //printf("[%i] (%ld)  killed  %s\n", job_id, (long) jobs[job_id - 1].pid, jobs[job_id - 1].name);
 }
 
 void handle_sigquit(int sig) {
@@ -29,15 +79,97 @@ void handle_sigquit(int sig) {
 }
 
 void install_signal_handlers() {
-    // TODO
+    // install SIGCHLD
+    struct sigaction chld;
+    chld.sa_handler = handle_sigchld;
+    chld.sa_flags = SA_RESTART;
+    sigfillset(&chld.sa_mask);
+    sigaction(SIGCHLD, &chld, NULL);
+
+    // install SIGINT
+    struct sigaction intt;
+    intt.sa_handler = handle_sigint;
+    intt.sa_flags = SA_RESTART;
+    sigfillset(&intt.sa_mask);
+    sigaction(SIGINT, &intt, NULL);
 }
 
 void spawn(const char **toks, bool bg) { // bg is true iff command ended with &
-    // TODO
+    sigset_t mask;  // sigset_t represent a signal set
+    sigfillset(&mask);  // all recongnized signals are excluded
+    sigprocmask(SIG_BLOCK, &mask, NULL);
+    pid_t p1 = fork();  // fork results in two concurrent processes
+
+    if (p1 == -1) {
+        sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        fprintf(stderr, "ERROR: cannot run %s\n", toks[0]);
+        exit(0);
+    }
+
+    if (p1 == 0) {
+        sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        int success = execvp(toks[0], toks);
+        if (success == -1) {
+            fprintf(stderr, "ERROR: cannot run %s\n", toks[0]);
+            exit(0);
+        } 
+    } else {
+        job_id = job_id + 1;
+        insert_jobs(toks, p1);
+        printf("[%i] (%ld)  %s\n", job_id, (long) p1, jobs[job_id - 1].name);
+        sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        if (bg) {
+            return;
+        } else {
+            while (jobs[0].terminated == false) {
+                sleep(1);
+            }
+        }
+    }
+}
+
+void insert_jobs(const char **toks, pid_t pid) {
+
+    // job id
+    curr.id = job_id;
+
+    // job id (string version)
+    char id_copy[5];
+    sprintf(id_copy, "%d", job_id);
+    curr.id_s = id_copy;
+
+    // pid 
+    curr.pid = pid;
+
+    // pid (string version)
+    int pid_length = 0;
+    long pid_temp = (long) pid;
+    while(pid_temp!=0)  {  
+        pid_temp = pid_temp/10;  
+        pid_length++;  
+    }  
+    pid_length++;  // account for ending string char
+    char pid_copy[pid_length];
+    sprintf(pid_copy, "%ld", (long) pid);
+    curr.pid_s = pid_copy;
+
+    printf("%s", curr.pid_s);
+
+    // name 
+    char *name_copy;
+    name_copy = malloc((strlen(toks[0]) + 1));
+    strcpy(name_copy, toks[0]);
+    curr.name = name_copy;
+
+    jobs[job_id - 1] = curr;  
 }
 
 void cmd_jobs(const char **toks) {
-    // TODO
+    for (int i = 0; i < job_id; i++) {
+        if (jobs[i].terminated == false) {
+            printf("[%d] (%ld)  running  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+        }
+    }
 }
 
 void cmd_fg(const char **toks) {
@@ -49,7 +181,37 @@ void cmd_bg(const char **toks) {
 }
 
 void cmd_slay(const char **toks) {
-    // TODO
+
+    if (toks[1][0] == (char)'%') {
+        //if (remaining != (toks[1] + (strlen(toks[1])) - 1)) {
+        //    fprintf(stderr, "ERROR: bad argument for slay: %s\n", toks[1]);
+        //} else {
+            for (int i = 0; i < job_id; i++) {
+                if (strcmp(jobs[i].id_s, toks[1] + 1) == 0) {
+                    jobs[i].terminated = true;
+                    kill(jobs[i].pid, SIGKILL);
+                    printf("[%d] (%ld)  killed  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+                    return;
+                }
+            }
+            fprintf(stderr, "ERROR: no job %s\n", toks[1]);
+        //}
+    } else {
+        //if(strlen(strtol(toks[1], &remaining, 10)) != strlen(toks[1])) {
+        //    fprintf(stderr, "ERROR: bad argument for slay: %s\n", toks[1]);
+        //} else {
+            for (int i = 0; i < job_id; i++) {
+                if (strcmp(jobs[i].pid_s, toks[1]) == 0) {
+                    jobs[i].terminated = true;
+                    kill(jobs[i].pid, SIGKILL);
+                    printf("[%d] (%ld)  killed  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+                    return;
+                }
+            }
+            fprintf(stderr, "ERROR: no PID %ld\n", (long) toks[1]);
+        //}
+    }
+
 }
 
 void cmd_quit(const char **toks) {
@@ -63,9 +225,26 @@ void cmd_quit(const char **toks) {
 void eval(const char **toks, bool bg) { // bg is true iff command ended with &
     assert(toks);
     if (*toks == NULL) return;
+
+    // quit
     if (strcmp(toks[0], "quit") == 0) {
         cmd_quit(toks);
-    // TODO: other commands
+
+    // jobs
+    } else if (strcmp(toks[0], "jobs") == 0) {
+        if (toks[1] != NULL) {
+            fprintf(stderr, "ERROR: jobs takes no arguments\n");
+        } else {
+            cmd_jobs(toks);
+        }
+
+    // slay, fg, bg
+    } else if (strcmp(toks[0], "slay") == 0) {
+        if (toks[1] == NULL || toks[3] != NULL) {
+            fprintf(stderr, "ERROR: slay takes exactly one argument\n");
+        } else {
+            cmd_slay(toks);
+        }
     } else {
         spawn(toks, bg);
     }
@@ -82,7 +261,7 @@ void parse_and_eval(char *s) {
         int t = 0;
 
         while (*s != '\0' && !end) {
-            while (*s == ' ' || *s == '\t' || *s == '\n') ++s;
+            while (*s == ' ' || *s == '\t' || *s == '\n') ++s;  
             if (*s != '&' && *s != ';' && *s != '\0') toks[t++] = s;
             while (strchr("&; \t\n", *s) == NULL) ++s;
             switch (*s) {
@@ -94,7 +273,7 @@ void parse_and_eval(char *s) {
                 end = true;
                 break;
             }
-            *s++ = '\0';
+            if (*s) *s++ = '\0';
         }
         toks[t] = NULL;
         eval(toks, bg);
--- task4/crash.c
+++ task4/crash.c
@@ -4,52 +4,286 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define MAXLINE 1024
 #define MAXJOBS 1024
 
+void cmd_jobs(const char**);
+void insert_jobs(const char**, pid_t);
+
 char **environ;
 
 // TODO: you will need some data structure(s) to keep track of jobs
+// Job object consisting job id, pid, and name of command
+typedef struct {
+    char *id_s;
+    int id;
+    char *pid_s;
+    pid_t pid;
+    char *name;
+    bool terminated;
+} job;
+
+job jobs[1024];  // array of pointers to job objects
+job curr;
+int job_id = 0;  // job id reference numbers
+int foreground_job = -1;
 
 void handle_sigchld(int sig) {
-    // TODO
+    sigset_t mask;  // sigset_t represent a signal set
+    sigfillset(&mask);  // all recongnized signals are excluded
+    pid_t sig_pid;
+
+    while ((sig_pid = waitpid(-1, 0, WNOHANG)) != -1) {
+        for (int i = 0; i < job_id; i++) {
+            if (jobs[i].pid == sig_pid) {
+                sigprocmask(SIG_BLOCK, &mask, NULL);
+                jobs[i].terminated = true;
+                sigprocmask(SIG_UNBLOCK, &mask, NULL);
+            }
+        }
+    }
+    
 }
 
 void handle_sigtstp(int sig) {
-    // TODO
+    
+    if (foreground_job != -1) {
+        if (jobs[foreground_job - 1].terminated == false) {
+            kill(jobs[foreground_job - 1].pid, SIGSTOP);
+        } else {
+            return;
+        }
+    } else {
+        return;
+    }
+    
 }
 
 void handle_sigint(int sig) {
-    // TODO
+    //sigset_t mask;  // sigset_t represent a signal set
+    //sigfillset(&mask);  // all recongnized signals are excluded
+    //pid_t sig_pid;
+
+    if (foreground_job != -1) {
+        if (jobs[foreground_job - 1].terminated == 0) {
+            write(STDOUT_FILENO, "[", sizeof("["));
+            write(STDOUT_FILENO, jobs[foreground_job - 1].id_s, sizeof(jobs[foreground_job - 1].id_s));
+            write(STDOUT_FILENO, "] (", sizeof("] ("));
+            write(STDOUT_FILENO, jobs[foreground_job - 1].pid_s, sizeof(jobs[foreground_job - 1].pid_s));
+            write(STDOUT_FILENO, ")  killed  ", sizeof(")  killed  "));
+            write(STDOUT_FILENO, jobs[foreground_job - 1].name, sizeof(jobs[foreground_job - 1].name));
+            write(STDOUT_FILENO, "\n", sizeof("\n"));
+            //sigprocmask(SIG_UNBLOCK, &mask, NULL);
+            kill(jobs[foreground_job - 1].pid, SIGINT);
+        } else {
+            return;
+        }
+    } else {
+        return;
+    }
+
 }
 
 void handle_sigquit(int sig) {
-    // TODO
+    
+    if (foreground_job != -1) {
+        if (jobs[foreground_job - 1].terminated == 0) {
+
+            write(STDOUT_FILENO, "[", sizeof("["));
+            write(STDOUT_FILENO, jobs[foreground_job - 1].id_s, sizeof(jobs[foreground_job - 1].id_s));
+            write(STDOUT_FILENO, "] (", sizeof("] ("));
+            write(STDOUT_FILENO, jobs[foreground_job - 1].pid_s, sizeof(jobs[foreground_job - 1].pid_s));
+            write(STDOUT_FILENO, ")  killed  ", sizeof(")  killed  "));
+            write(STDOUT_FILENO, jobs[foreground_job - 1].name, sizeof(jobs[foreground_job - 1].name));
+            write(STDOUT_FILENO, "\n", sizeof("\n"));
+            kill(jobs[foreground_job - 1].pid, SIGQUIT);
+            return;
+        } else {
+            _exit(0);
+        }
+    } else {
+        _exit(0);
+    }
+    _exit(0);
+
 }
 
 void install_signal_handlers() {
-    // TODO
+    // install SIGCHLD
+    struct sigaction chld;
+    chld.sa_handler = handle_sigchld;
+    chld.sa_flags = SA_RESTART;
+    sigfillset(&chld.sa_mask);
+    sigaction(SIGCHLD, &chld, NULL);
+
+    // install SIGINT
+    struct sigaction intt;
+    intt.sa_handler = handle_sigint;
+    intt.sa_flags = SA_RESTART;
+    sigfillset(&intt.sa_mask);
+    sigaction(SIGINT, &intt, NULL);
+
+    // install SIGQUIT
+    struct sigaction quit;
+    quit.sa_handler = handle_sigquit;
+    quit.sa_flags = SA_RESTART;
+    sigemptyset(&quit.sa_mask);
+    sigaction(SIGQUIT, &quit, NULL);
 }
 
 void spawn(const char **toks, bool bg) { // bg is true iff command ended with &
-    // TODO
+    sigset_t mask;  // sigset_t represent a signal set
+    sigfillset(&mask);  // all recongnized signals are excluded
+    sigprocmask(SIG_BLOCK, &mask, NULL);
+    pid_t p1 = fork();  // fork results in two concurrent processes
+
+    if (p1 == -1) {
+        fprintf(stderr, "ERROR: cannot run %s\n", toks[0]);
+        sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        exit(0);
+    }
+    if (p1 == 0) {
+        sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        int success = execvp(toks[0], toks);
+        if (success == -1) {
+            fprintf(stderr, "ERROR: cannot run %s\n", toks[0]);
+        } 
+        exit(0);
+    } else {
+        job_id = job_id + 1;
+        insert_jobs(toks, p1);
+        printf("[%i] (%ld)  %s\n", job_id, (long) p1, jobs[job_id - 1].name);
+        sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        if (bg) {
+            return;
+        }
+        if (!bg) {
+            while (jobs[foreground_job - 1].terminated == false) {
+                sleep(0.1);
+            }
+        }
+    }
+
+}
+
+void insert_jobs(const char **toks, pid_t pid) {
+
+    // job id
+    jobs[job_id - 1].id = job_id;
+
+    // job id (string version)
+    int id_length = 0;  // length of job id
+    int id_temp = job_id;  // temporary id used to count number of digits
+    while(id_temp!=0)  {  
+        id_temp = id_temp/10;  
+        id_length++;  
+    }  
+    id_length++;
+    jobs[job_id - 1].id_s = malloc(id_length * sizeof(char*));
+    sprintf(jobs[job_id - 1].id_s, "%d", job_id);
+
+    // pid 
+    jobs[job_id - 1].pid = pid;
+
+    // pid (string version)
+    int pid_length = 0;
+    int pid_temp = (int) pid;
+    while(pid_temp!=0)  {  
+        pid_temp = pid_temp/10;  
+        pid_length++;  
+    }  
+    pid_length++;  // account for ending string char
+    jobs[job_id - 1].pid_s = malloc(pid_length * sizeof(char*));
+    sprintf(jobs[job_id - 1].pid_s, "%ld", (long) pid);
+
+    // name 
+    char *name_copy;
+    name_copy = malloc((strlen(toks[0]) + 1));
+    strcpy(name_copy, toks[0]);
+    jobs[job_id - 1].name = name_copy;
+
+    // terminated
+    jobs[job_id - 1].terminated = false;
+
+    // update foreground job if appropriate 
+    if (!toks[2]) {
+        foreground_job = jobs[job_id - 1].id;
+    }
+
 }
 
 void cmd_jobs(const char **toks) {
-    // TODO
+    for (int i = 0; i < job_id; i++) {
+        if (jobs[i].terminated == false) {
+            printf("[%d] (%ld)  running  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+        }
+    }
 }
 
 void cmd_fg(const char **toks) {
-    // TODO
+
+    if (toks[1][0] == (char)'%') {
+        for (int i = 0; i < job_id; i++) {
+            if (strcmp(jobs[i].id_s, toks[1] + 1) == 0) {
+                foreground_job = jobs[i].id;
+                while (jobs[foreground_job - 1].terminated == false) {
+                    sleep(0.1);
+                }
+                return;
+            }
+        }
+        fprintf(stderr, "ERROR: no job %s\n", toks[1]);
+    } else {
+        for (int i = 0; i < job_id; i++) {
+            if (strcmp(jobs[i].pid_s, toks[1]) == 0) {
+                foreground_job = jobs[i].id;
+                while (jobs[foreground_job - 1].terminated == false) {
+                    sleep(0.1);
+                }
+                return;
+            }
+        }
+        fprintf(stderr, "ERROR: no PID %ld\n", (long) toks[1]);
+    }
 }
 
 void cmd_bg(const char **toks) {
-    // TODO
+
 }
 
 void cmd_slay(const char **toks) {
-    // TODO
+    sigset_t mask;  // sigset_t represent a signal set
+    sigfillset(&mask);  // all recongnized signals are excluded
+
+    if (toks[1][0] == (char)'%') {
+        for (int i = 0; i < job_id; i++) {
+            if (strcmp(jobs[i].id_s, toks[1] + 1) == 0) {
+                sigprocmask(SIG_BLOCK, &mask, NULL);
+                jobs[i].terminated = true;
+                kill(jobs[i].pid, SIGKILL);
+                printf("[%d] (%ld)  killed  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+                sigprocmask(SIG_UNBLOCK, &mask, NULL);
+                return;
+            }
+        }
+        fprintf(stderr, "ERROR: no job %s\n", toks[1]);
+    } else {
+        for (int i = 0; i < job_id; i++) {
+            if (strcmp(jobs[i].pid_s, toks[1]) == 0) {
+                sigprocmask(SIG_BLOCK, &mask, NULL);
+                jobs[i].terminated = true;
+                kill(jobs[i].pid, SIGKILL);
+                printf("[%d] (%ld)  killed  %s\n", jobs[i].id, (long) jobs[i].pid, jobs[i].name);
+                sigprocmask(SIG_UNBLOCK, &mask, NULL);
+                return;
+            }
+        }
+        fprintf(stderr, "ERROR: no PID %ld\n", (long) toks[1]);
+    }
+
 }
 
 void cmd_quit(const char **toks) {
@@ -63,9 +297,32 @@ void cmd_quit(const char **toks) {
 void eval(const char **toks, bool bg) { // bg is true iff command ended with &
     assert(toks);
     if (*toks == NULL) return;
+
+    // quit
     if (strcmp(toks[0], "quit") == 0) {
         cmd_quit(toks);
-    // TODO: other commands
+
+    // jobs
+    } else if (strcmp(toks[0], "jobs") == 0) {
+        if (toks[1] != NULL) {
+            fprintf(stderr, "ERROR: jobs takes no arguments\n");
+        } else {
+            cmd_jobs(toks);
+        }
+
+    // slay, fg, bg
+    } else if (strcmp(toks[0], "slay") == 0) {
+        if (toks[1] == NULL || toks[3] != NULL) {
+            fprintf(stderr, "ERROR: slay takes exactly one argument\n");
+        } else {
+            cmd_slay(toks);
+        }
+    } else if (strcmp(toks[0], "fg") == 0) {
+        if (toks[1] == NULL || toks[3] != NULL) {
+            fprintf(stderr, "ERROR: fg takes exactly one argument\n");
+        } else {
+            cmd_fg(toks);
+        }
     } else {
         spawn(toks, bg);
     }
@@ -82,7 +339,7 @@ void parse_and_eval(char *s) {
         int t = 0;
 
         while (*s != '\0' && !end) {
-            while (*s == ' ' || *s == '\t' || *s == '\n') ++s;
+            while (*s == ' ' || *s == '\t' || *s == '\n') ++s;  
             if (*s != '&' && *s != ';' && *s != '\0') toks[t++] = s;
             while (strchr("&; \t\n", *s) == NULL) ++s;
             switch (*s) {
@@ -94,7 +351,7 @@ void parse_and_eval(char *s) {
                 end = true;
                 break;
             }
-            *s++ = '\0';
+            if (*s) *s++ = '\0';
         }
         toks[t] = NULL;
         eval(toks, bg);
